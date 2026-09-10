1f86dbf1e83610b62fb15e467978b8fd63be816b
Carlos Velasco <xtreemze@gmail.com>
2026-09-10 20:03:06 +0800

    feat(simulation): expose pure domain API

--- packages/simulation/src/index.ts
+++ packages/simulation/src/index.ts
@@ -1,1 +1,9 @@
 export const SIMULATION_SCHEMA_VERSION = 1 as const;
+
+export * from "./environment.js";
+export * from "./model.js";
+export * from "./primitives.js";
+export * from "./rng.js";
+export * from "./rules.js";
+export * from "./simulate.js";
+export * from "./state.js";
