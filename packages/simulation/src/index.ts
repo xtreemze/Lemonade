1f6239c5dd4effd3c50d702f85ac69cf75909cc6
Carlos Velasco <xtreemze@gmail.com>
2026-09-10 20:52:08 +0800

    feat(simulation): export finance rules and bump schema

--- packages/simulation/src/index.ts
+++ packages/simulation/src/index.ts
@@ -1,6 +1,7 @@
-export const SIMULATION_SCHEMA_VERSION = 1 as const;
+export const SIMULATION_SCHEMA_VERSION = 2 as const;
 
 export * from "./environment.js";
+export * from "./finance.js";
 export * from "./model.js";
 export * from "./primitives.js";
 export * from "./rng.js";
