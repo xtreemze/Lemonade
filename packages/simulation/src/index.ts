fefffc76c3fd7d8b2e037de886837a25a57b6a37
Carlos Velasco <xtreemze@gmail.com>
2026-09-21 15:32:32 +0800

    lint: mark model barrel export as type-only

--- packages/simulation/src/index.ts
+++ packages/simulation/src/index.ts
@@ -1,7 +1,7 @@
 export * from "./certification.js";
 export * from "./environment.js";
 export * from "./finance.js";
-export * from "./model.js";
+export type * from "./model.js";
 export * from "./primitives.js";
 export * from "./rng.js";
 export * from "./rules.js";
