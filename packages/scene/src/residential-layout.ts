b96d0f11c60a0badfe2d695d4dd4b38ab90a8e7a
Carlos Velasco <xtreemze@gmail.com>
2026-09-22 20:31:30 +0800

    Face deep residential row toward neighborhood street

--- packages/scene/src/residential-layout.ts
+++ packages/scene/src/residential-layout.ts
@@ -576,7 +576,7 @@ export const generateResidentialLayout = (seed = DEFAULT_RESIDENTIAL_SEED): Resi
       3_000,
       -72,
       [-94, -82, -70, -43, -31, -7, 6, 31, 44, 70, 83, 95],
-      true,
+      false,
     ),
   ]);
   const allProperties = [...front, ...middle, ...back, ...outer];
