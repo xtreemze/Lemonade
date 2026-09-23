f6ff109ae61393a383808a823375be0f69d9fd6e
Carlos Velasco <xtreemze@gmail.com>
2026-09-24 02:03:23 +0800

    feat(simulation): export neighborhood occurrences

--- packages/simulation/src/index.ts
+++ packages/simulation/src/index.ts
@@ -3,6 +3,7 @@ export * from "./certification.js";
 export * from "./environment.js";
 export * from "./finance.js";
 export * from "./legacy.js";
+export * from "./occurrences.js";
 export type * from "./model.js";
 export * from "./primitives.js";
 export * from "./rng.js";
