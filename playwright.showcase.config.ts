19bdf63799fda95206fc1405b946b8f9ed3af653
Carlos Velasco <xtreemze@gmail.com>
2026-09-26 08:01:51 +0800

    chore(showcase): let deterministic clock own frame cadence

--- playwright.showcase.config.ts
+++ playwright.showcase.config.ts
@@ -27,8 +27,6 @@ export default defineConfig({
         "--disable-background-timer-throttling",
         "--disable-renderer-backgrounding",
         "--disable-backgrounding-occluded-windows",
-        "--disable-frame-rate-limit",
-        "--disable-gpu-vsync",
       ],
     },
   },
